fe79c9ddbd6270ade23609d471b2124cd57418da
oskretc <otorresbaca@gmail.com>
2021-12-17 12:40:09 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Confused)
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+    	
+    } else if (false) {
+    	
+    }
+})
 basic.forever(function () {
 	
 })
