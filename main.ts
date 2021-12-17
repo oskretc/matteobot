b5d44e28bba71412728dc5e34a5f27a42e34e077
oskretc <otorresbaca@gmail.com>
2021-12-17 12:43:29 +0100

    adding controls

--- main.ts
+++ main.ts
@@ -3,11 +3,18 @@ bluetooth.onBluetoothConnected(function () {
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Confused)
+    kitronik_servo_lite.stop()
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
-    	
-    } else if (false) {
+        kitronik_servo_lite.forward()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
+        kitronik_servo_lite.stop()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        kitronik_servo_lite.backward()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
+        kitronik_servo_lite.stop()
+    } else {
     	
     }
 })
