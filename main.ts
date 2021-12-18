3e6f8b9ed175a48d3b5c5cc611b276f480445374
oskretc <otorresbaca@gmail.com>
2021-12-18 21:35:11 +0100

    ble working

--- main.ts
+++ main.ts
@@ -8,14 +8,18 @@ bluetooth.onBluetoothDisconnected(function () {
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
         kitronik_servo_lite.forward()
+        basic.showIcon(IconNames.Pitchfork)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
         kitronik_servo_lite.stop()
+        basic.clearScreen()
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
         kitronik_servo_lite.backward()
+        basic.showIcon(IconNames.Sword)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
         kitronik_servo_lite.stop()
+        basic.clearScreen()
     } else {
-    	
+        basic.showIcon(IconNames.Heart)
     }
 })
 basic.forever(function () {
